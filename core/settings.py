0fb5fde79c9e068b492a45665c2ccc4103b14a28
giyos <giyos@giyoss-MacBook-Pro.local>
2026-07-24 20:10:10 +0500

    add change

--- core/settings.py
+++ core/settings.py
@@ -180,6 +180,7 @@ SOCIALACCOUNT_LOGIN_ON_GET = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
